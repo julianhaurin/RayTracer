7bc4120efb0d2bfd928a32f0e5f48b0ad0e6cbe4
Julian Haurin <julian.haurin@gmail.com>
2023-09-21 18:27:03 -0400

    Initial commit

---
+++ RayTracer/src/main.cpp
@@ -0,0 +1,17 @@
+
+// Ray Tracer - Julian Haurin - 9/21/23
+
+// Sources:
+// raytracing.github.io/books/RayTracingInOneWeekend.html
+
+// Notes:
+//
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello, World!" << std::endl;
+	return EXIT_SUCCESS;
+
+}
